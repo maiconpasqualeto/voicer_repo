3c2aa87c9a65b01e415786174d892c1684308d70
maicon <maiconpas@yahoo.com.br>
2015-04-17 17:23:21 -0400

    criação das telas de base para o produto final

--- Voicer/src/br/com/sixinf/voicer/telas/LoginActivity.java
+++ Voicer/src/br/com/sixinf/voicer/telas/LoginActivity.java
@@ -1,9 +1,16 @@
+/**
+ * 
+ */
 package br.com.sixinf.voicer.telas;
 
 import org.doubango.ngn.events.NgnInviteEventArgs;
 import org.doubango.ngn.events.NgnRegistrationEventArgs;
 
+import br.com.sixinf.voicer.R;
+import br.com.sixinf.voicer.receivers.RegistrationBroadcastReceiver;
+import br.com.sixinf.voicer.sip.VoicerFacade;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,11 +19,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import br.com.sixinf.voicer.R;
-import br.com.sixinf.voicer.receivers.RegistrationBroadcastReceiver;
-import br.com.sixinf.voicer.sip.VoicerFacade;
 
-public class SetupActivity extends Activity implements IUpdateStatus {
+/**
+ * @author maicon
+ *
+ */
+public class LoginActivity extends Activity implements IUpdateStatus {
 	
 	private EditText txtUsuario;
 	private EditText txtSenha;
@@ -26,17 +34,18 @@ public class SetupActivity extends Activity implements IUpdateStatus {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_setup);
+		
+		setContentView(R.layout.activity_login);
 		
 		// Inicializa a fachada e a engine do Audio
-		/*VoicerFacade.getInstance().setMainActivity(this);
-		VoicerFacade.getInstance().startSipService();*/
+		VoicerFacade.getInstance().setMainActivity(this);
+		VoicerFacade.getInstance().startSipService();
 		
-		txtUsuario = (EditText) findViewById(R.id.txtUsuario);
-		txtSenha = (EditText) findViewById(R.id.txtSenha);
-		txtStatus = (TextView) findViewById(R.id.txtStatus);
+		txtUsuario = (EditText) findViewById(R.id.login_txtUsuario);
+		txtSenha = (EditText) findViewById(R.id.login_txtSenha);
+		txtStatus = (TextView) findViewById(R.id.login_lblStatus);
 		
-		/*Button btnLogar = (Button) findViewById(R.id.btnLogar);
+		Button btnLogar = (Button) findViewById(R.id.login_btnLogar);
 		btnLogar.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -51,10 +60,11 @@ public class SetupActivity extends Activity implements IUpdateStatus {
 		final IntentFilter intentFilter = new IntentFilter();
 		intentFilter.addAction(NgnRegistrationEventArgs.ACTION_REGISTRATION_EVENT);
 		intentFilter.addAction(NgnInviteEventArgs.ACTION_INVITE_EVENT);
-		registerReceiver(regBroadcastReceiver, intentFilter);*/
+		registerReceiver(regBroadcastReceiver, intentFilter);
 		
 	}
-
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -65,11 +75,13 @@ public class SetupActivity extends Activity implements IUpdateStatus {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		int id = item.getItemId();
+		
 		if (id == R.id.login_mnuSetup) {
-			
-			
+			Intent it = new Intent(this, SetupActivity.class);			
+			startActivity(it);
 			return true;
 		}
+		
 		return super.onOptionsItemSelected(item);
 	}
 	
@@ -89,4 +101,5 @@ public class SetupActivity extends Activity implements IUpdateStatus {
 			}
 		});
 	}
+
 }
--- Voicer/src/br/com/sixinf/voicer/telas/VoicerActivity.java
+++ Voicer/src/br/com/sixinf/voicer/telas/VoicerActivity.java
@@ -79,11 +79,8 @@ public class VoicerActivity extends Activity implements IUpdateStatus {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.login_mnuSetup) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
